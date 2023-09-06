9023b1b33205fcf921de49309c01f0e429e563a0
RubenErhardt <rubengamebattles@outlook.com>
2023-09-06 15:13:29 +0200

    4e versie
    4e versie

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+// Function to open the YouTube video when the link is clicked
+function openYouTubeVideo(event) {
+  // Prevent the default link behavior to avoid redirection
+  event.preventDefault();
 
-function expand () {
-  document.body.classList.toggle('expand')
+  // Replace 'VIDEO_ID' with your actual YouTube video ID
+  const videoId = 'joguOtdayb0&t=2s';
+
+  // Open the YouTube video link in a new tab
+  window.open(`https://www.youtube.com/watch?v=${videoId}`, '_blank');
 }
+
+// Attach a click event listener to the "Watch My Video" link
+document.addEventListener("DOMContentLoaded", function () {
+  const watchVideoLink = document.getElementById("watchVideoLink");
+  watchVideoLink.addEventListener("click", openYouTubeVideo);
+});
