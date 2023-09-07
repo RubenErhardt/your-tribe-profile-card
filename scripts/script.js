be48b95aedf3cd8b1230a74e7bca75f38a6bccbc
RubenErhardt <rubengamebattles@outlook.com>
2023-09-07 12:22:11 +0200

    6e

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,23 @@
-// Function to open the YouTube video when the link is clicked
 function openYouTubeVideo(event) {
-  // Prevent the default link behavior to avoid redirection
+
   event.preventDefault();
 
-  // Replace 'VIDEO_ID' with your actual YouTube video ID
   const videoId = 'joguOtdayb0&t=2s';
 
-  // Open the YouTube video link in a new tab
   window.open(`https://www.youtube.com/watch?v=${videoId}`, '_blank');
 }
 
-// Attach a click event listener to the "Watch My Video" link
 document.addEventListener("DOMContentLoaded", function () {
   const watchVideoLink = document.getElementById("watchVideoLink");
   watchVideoLink.addEventListener("click", openYouTubeVideo);
 });
+
+// Zoek het kaartelement op basis van de ID
+const card = document.getElementById('card');
+
+// Voeg een click-event luisteraar toe aan het kaartelement
+card.addEventListener('click', function () {
+    // Toggle de 'flipped' klasse om de kaart om te draaien
+    card.classList.toggle('flipped');
+});
+
