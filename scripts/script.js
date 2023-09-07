df69a64b8837541abe9ce6c2efef2481f83d59aa
RubenErhardt <rubengamebattles@outlook.com>
2023-09-07 12:24:55 +0200

    7

--- scripts/script.js
+++ scripts/script.js
@@ -11,13 +11,3 @@ document.addEventListener("DOMContentLoaded", function () {
   const watchVideoLink = document.getElementById("watchVideoLink");
   watchVideoLink.addEventListener("click", openYouTubeVideo);
 });
-
-// Zoek het kaartelement op basis van de ID
-const card = document.getElementById('card');
-
-// Voeg een click-event luisteraar toe aan het kaartelement
-card.addEventListener('click', function () {
-    // Toggle de 'flipped' klasse om de kaart om te draaien
-    card.classList.toggle('flipped');
-});
-
